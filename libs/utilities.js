1729ac6f28971c287717f521a229c187ba1a06cd
Hiroki Tanaka <h.tanaka@salesforce.com>
2017-04-26 17:21:26 +0900

    bug fix

--- libs/utilities.js
+++ libs/utilities.js
@@ -1,10 +1,10 @@
 exports.getResponder = function() {
-return require('./tmp/responder');
+return require('../tmp/responder.json');
 }
 
 exports.setResponder = function(responder) {
 var fs = require('fs');
-fs.writeFile( './tmp/responder.js', JSON.stringify( responder ), 'utf8', function(){
+fs.writeFile( './tmp/responder.json', JSON.stringify( responder ), 'utf8', function(){
   console.log('responder updated: ' + JSON.stringify( responder ));
 });
 }
