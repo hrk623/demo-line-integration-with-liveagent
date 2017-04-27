cacf517e99b2a64c2f06ee13bdcd9fac8ef6a837
Hiroki Tanaka <h.tanaka@salesforce.com>
2017-04-27 19:42:48 +0900

    bug fix

--- libs/utilities.js
+++ libs/utilities.js
@@ -183,6 +183,7 @@ exports.getContent = function(line, message, callback) {
       length: response.headers["content-length"],
       data: body
     };
+    console.log(content);
     callback(content);
   });
 };
