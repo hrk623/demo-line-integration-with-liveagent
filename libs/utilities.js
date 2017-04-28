11ba535c6eae7848e048e8af89c96c7f6be9d1ae
Hiroki Tanaka <h.tanaka@salesforce.com>
2017-04-28 12:18:57 +0900

    bug fix

--- libs/utilities.js
+++ libs/utilities.js
@@ -179,11 +179,11 @@ exports.getContent = function(line, message, callback) {
   request(options, function (error, response, body) {
       // body is the decompressed response body 
       console.log('server encoded the data as: ' + (response.headers['content-encoding'] || 'identity'))
-      console.log('the decoded data is: ' + body)
+      console.log('the decoded data is: ')
     }
   ).on('data', function(data) {
     // decompressed data as it is received 
-    console.log('decoded chunk: ' + data)
+    console.log('decoded chunk: ')
   })
   .on('response', function(response) {
     // unmodified http.IncomingMessage object 
